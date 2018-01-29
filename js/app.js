491704d00e69fea7f415ae783bcff5a4484184b0
Ilya <ilyaivanov07@users.noreply.github.com>
2018-01-29 12:30:57 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
 /*
+The game randomly shuffles the cards. A user wins once all cards have successfully been matched.
+
+When a user wins the game, a modal appears to congratulate the player and ask if they want to play again. 
+It should also tell the user how much time it took to win the game, and what the star rating was.
+
+A restart button allows the player to reset the game board, the timer, and the star rating.
+
+The game displays a star rating (from 1-3) that reflects the player's performance. At the beginning of a game, 
+it should display 3 stars. After some number of moves, it should change to a 2 star rating. After a few more moves, 
+it should change to a 1 star rating. The number of moves needed to change the rating is up to you, but it should happen at some point.
+
+When the player starts a game, a displayed timer should also start. Once the player wins the game, the timer stops.
+
+Game displays the current number of moves a user has made.
+
+*/
+
+/*
  * Create a list that holds all of your cards
  */
 
@@ -19,7 +37,13 @@ function switchCard(selectedCard) {
 
 	// update the move counter 
 	document.getElementsByClassName("moves")[0].innerHTML = parseInt(document.getElementsByClassName("moves")[0].innerHTML) + 1;
-
+	let moves = parseInt(document.getElementsByClassName("moves")[0].innerHTML);
+	
+	if (moves === 5 || moves === 20) {
+		reduceStars();		
+	}
+	
+	
 	// if card is not displayed 
 	if (selectedCard.className === "card") { 
 		// display the card
@@ -63,6 +87,12 @@ function closeCards(selectedCard, prevCard) {
 	prevCard.setAttribute("class", "card");
 }
 
+
+function reduceStars() {
+	const stars = document.getElementsByClassName("stars")[0];
+    const starHTML = '<li><i class="fa fa-star"></i></li>';
+    stars.innerHTML = starHTML.repeat(stars.children.length - 1); 
+}
 
 function shuffleCards(array) {
 	cardStyles = shuffle(array);
@@ -118,4 +148,6 @@ function shuffle(array) {
  
  // initialize the game
  initGame();
+ 
+ 
  
